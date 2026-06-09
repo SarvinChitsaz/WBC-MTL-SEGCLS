1773b6bfcc93a0d8dbadafe774655b1e4d8e8c58
Sarvin Chitsaz <sarvin.chitsaz@gmail.com>
2026-06-09 22:30:14 +0330

    Update config.py

---
+++ configs/config.py
@@ -0,0 +1,9 @@
+import os
+
+ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+dataset1_dir = os.path.join(ROOT, 'data', 'Dataset 1')
+dataset2_dir = os.path.join(ROOT, 'data', 'Dataset 2')
+
+csv1_path = os.path.join(ROOT, 'data', 'Class Labels of Dataset 1.csv')
+csv2_path = os.path.join(ROOT, 'data', 'Class Labels of Dataset 2.csv')
