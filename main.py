6e730654395de01f988caba61bb05f2b32cda289
Sarvin Chitsaz <sarvin.chitsaz@gmail.com>
2026-06-10 00:48:57 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,53 +1,54 @@
+from configs.config import *
+from data.prepare_data import prepare_dataset
+from data.dataset import get_dataloaders
+from data.transforms import get_train_transforms, get_test_transforms
+from models.model import UNetResNet34MultiTask
 import torch
 import torch.nn as nn
-from configs.config import *
-from data.dataset import get_dataloaders
-from data.transforms import train_transform, test_transform
-from models.model import UNetResNet34MultiTask
+import torch.optim as optim
 
 
 def main():
 
-    base_path = DATA_DIR
-
-    train_loader, test_loader, n_classes_cls, le = get_dataloaders(
-        base_path=base_path,
-        batch_size=BATCH_SIZE
+    train_df, test_df, le = prepare_dataset(
+        csv1_path, csv2_path, dataset1_dir, dataset2_dir
     )
+
+    train_loader, test_loader = get_dataloaders(train_df, test_df, BATCH_SIZE)
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     model = UNetResNet34MultiTask(
         n_classes_seg=3,
-        n_classes_cls=n_classes_cls
+        n_classes_cls=len(le.classes_)
     ).to(device)
 
-    optimizer = torch.optim.Adam(model.parameters(), lr=LR)
+    optimizer = optim.Adam(model.parameters(), lr=LR)
+
     criterion_seg = nn.CrossEntropyLoss()
     criterion_cls = nn.CrossEntropyLoss()
 
     for epoch in range(EPOCHS):
         model.train()
-
         total_loss = 0
 
         for batch in train_loader:
-            x = batch["image"].to(device)
-            y_seg = batch["mask"].to(device)
-            y_cls = batch["label"].to(device)
+            images = batch["image"].to(device)
+            masks = batch["mask"].to(device)
+            labels = batch["label"].to(device)
 
             optimizer.zero_grad()
 
-            seg_out, cls_out = model(x)
+            seg_out, cls_out = model(images)
 
-            loss = criterion_seg(seg_out, y_seg) + CLS_LOSS_WEIGHT * criterion_cls(cls_out, y_cls)
+            loss = criterion_seg(seg_out, masks) + CLS_LOSS_WEIGHT * criterion_cls(cls_out, labels)
 
             loss.backward()
             optimizer.step()
 
             total_loss += loss.item()
 
-        print(f"Epoch {epoch+1} | Loss: {total_loss/len(train_loader):.4f}")
+        print(f"Epoch {epoch+1} Loss: {total_loss / len(train_loader):.4f}")
 
 
 if __name__ == "__main__":
