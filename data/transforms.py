13afd4a3578c2e8136cedab900dfbb3196b7591a
Sarvin Chitsaz <sarvin.chitsaz@gmail.com>
2026-06-10 00:30:39 +0330

    Update transforms.py

--- data/transforms.py
+++ data/transforms.py
@@ -1,29 +1,23 @@
 import albumentations as A
 from albumentations.pytorch import ToTensorV2
 
-train_transform = A.Compose([
-    A.Resize(256, 256),
-    A.HorizontalFlip(p=0.5),
-    A.RandomRotate90(p=0.3),
-    A.ShiftScaleRotate(p=0.4),
-    A.RandomBrightnessContrast(p=0.3),
-    A.Normalize(),
-    ToTensorV2()
-])
 
-minority_transform = A.Compose([
-    A.Resize(256, 256),
-    A.HorizontalFlip(p=0.7),
-    A.RandomRotate90(p=0.5),
-    A.ShiftScaleRotate(p=0.6),
-    A.RandomBrightnessContrast(p=0.5),
-    A.GaussianBlur(p=0.2),
-    A.Normalize(),
-    ToTensorV2()
-])
+def get_transforms(image_size=256):
 
-test_transform = A.Compose([
-    A.Resize(256, 256),
-    A.Normalize(),
-    ToTensorV2()
-])
+    train_transform = A.Compose([
+        A.Resize(image_size, image_size),
+        A.HorizontalFlip(p=0.5),
+        A.RandomRotate90(p=0.3),
+        A.ShiftScaleRotate(p=0.3),
+        A.RandomBrightnessContrast(p=0.3),
+        A.Normalize(),
+        ToTensorV2()
+    ])
+
+    test_transform = A.Compose([
+        A.Resize(image_size, image_size),
+        A.Normalize(),
+        ToTensorV2()
+    ])
+
+    return train_transform, test_transform
