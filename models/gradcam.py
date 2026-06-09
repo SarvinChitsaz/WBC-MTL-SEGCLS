6e42be2dbb7d4fafe353ea977907649d87e5799b
Sarvin Chitsaz <sarvin.chitsaz@gmail.com>
2026-06-10 00:47:47 +0330

    Update gradcam.py

--- models/gradcam.py
+++ models/gradcam.py
@@ -1,12 +1,13 @@
 import torch
 import torch.nn.functional as F
-import numpy as np
 import cv2
+import numpy as np
 
 
 class GradCAM:
     def __init__(self, model, target_layer):
         self.model = model
+        self.model.eval()
         self.target_layer = target_layer
         self.activations = None
         self.gradients = None
@@ -14,24 +15,24 @@ class GradCAM:
         self.target_layer.register_forward_hook(self.forward_hook)
         self.target_layer.register_full_backward_hook(self.backward_hook)
 
-    def forward_hook(self, module, inp, out):
-        self.activations = out.detach()
+    def forward_hook(self, module, input, output):
+        self.activations = output.detach()
 
-    def backward_hook(self, module, grad_in, grad_out):
-        self.gradients = grad_out[0].detach()
+    def backward_hook(self, module, grad_input, grad_output):
+        self.gradients = grad_output[0].detach()
 
     def generate(self, x, class_idx):
         self.model.zero_grad()
-
         _, out = self.model(x)
+
         score = out[:, class_idx]
         score.backward(retain_graph=True)
 
         weights = self.gradients.mean(dim=(2, 3), keepdim=True)
         cam = (weights * self.activations).sum(dim=1)
-
         cam = F.relu(cam)[0]
-        cam = cam - cam.min()
-        cam = cam / (cam.max() + 1e-8)
+
+        cam -= cam.min()
+        cam /= (cam.max() + 1e-8)
 
         return cam.cpu().numpy()
